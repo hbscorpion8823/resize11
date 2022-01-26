9d0b3cfe848032feef11c625221c9699058c8d84
hbscorpion8823 <hbculture@jcom.zaq.ne.jp>
2022-01-26 16:34:42 +0900

    背景透過＋コメントを日本語で丁寧に記述

--- cmd/resize11.go
+++ cmd/resize11.go
@@ -38,28 +38,46 @@ func main() {
 	width := mw1.GetImageWidth()
 	height := mw1.GetImageHeight()
 	maxNum := int(math.Max(float64(width), float64(height)))
+
+	// 背景用のMagickWand
+	mw2 := imagick.NewMagickWand()
+	defer mw2.Destroy()
+
+	// 1x1のキャンバスを灰色で描画
+	pw := imagick.NewPixelWand()
+	defer pw.Destroy()
+	pw.SetColor("gray")
+	mw2.NewImage(1, 1, pw)
+
+	// キャンバスサイズを変更
+	err = mw2.ExtentImage(uint(maxNum), uint(maxNum), 0, 0)
+	if err != nil {
+		panic(err)
+	}
 	fmt.Printf("Width: %d --> %d \n", width, maxNum)
 	fmt.Printf("Height: %d --> %d \n", height, maxNum)
 
-	// resize canvas
-	err = mw1.ExtentImage(uint(maxNum), uint(maxNum), 0, 0)
+	// 背景を透過
+	mw2.SetImageAlphaChannel(imagick.ALPHA_CHANNEL_TRANSPARENT)
+
+	// 画像を重ねる
+	err = mw2.CompositeImage(mw1, mw1.GetImageCompose(), 0, 0)
 	if err != nil {
 		panic(err)
 	}
 
-	// dst dir
+	// 出力先ディレクトリ
 	dstDir := filepath.Dir(srcPath)
-	// dst extension
+	// 出力ファイル拡張子
 	dstExt := filepath.Ext(srcPath)
 
-	// dst file name
+	// 現在日時から出力ファイル名を決定
 	nowTime := time.Now()
 	dstFilename := nowTime.Format("2006-01-02-150405")
-
 	dstPath := filepath.Join(dstDir, dstFilename+dstExt)
 
 	// ファイル出力
-	mw1.WriteImage(dstPath)
+	mw2.WriteImage(dstPath)
 
 	// 終了メッセージ出力
 	fmt.Println(dstPath, " output complete!!")
